1b5e8c5a2898ace016ca166d5911d54714a4e2e4
GitLab CI/CD - Paloma <palomaguenes@tecgraf.puc-rio.br>
2023-05-11 05:50:54 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.74bd3791058850a0ccea21264033a679.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07e03653b59d1006086",
+    "url": "/static/css/main.3d837567.chunk.css"
+  },
+  {
+    "revision": "b07e03653b59d1006086",
+    "url": "/static/js/main.b07e0365.chunk.js"
+  },
+  {
+    "revision": "a31e19c2ecb92692751e",
+    "url": "/static/js/1.a31e19c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "1456a6be906dcfe7818c47b28b409501",
+    "url": "/static/media/person.1456a6be.svg"
+  },
+  {
+    "revision": "0fc053ba9ccf6f9d63d45098678875bf",
+    "url": "/static/media/search.0fc053ba.svg"
+  },
+  {
+    "revision": "c9c2fd159a339e59c77962878b1676bd",
+    "url": "/static/media/person-add.c9c2fd15.svg"
+  },
+  {
+    "revision": "576f26ff09a3275d9da0621e6b34898d",
+    "url": "/static/media/cancel.576f26ff.svg"
+  },
+  {
+    "revision": "5e28144e6aa47f158423654f628b9b71",
+    "url": "/index.html"
+  }
+];
